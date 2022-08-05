73988034c944ce92ad7525b34de981abeaee835c
Friedrich von Never <friedrich@fornever.me>
2022-08-05 21:16:04 +0200

    Add project sources

---
+++ Listener/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listener
+{
+    class Program
+    {
+        private static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void Log(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        
+        static async Task Main(string[] args)
+        {
+            int port = args.Length == 0 ? 8222 : int.Parse(args[0], CultureInfo.InvariantCulture);
+            using var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endpoint = new IPEndPoint(IPAddress.Loopback, port);
+            serverSocket.Bind(endpoint);
+            serverSocket.Listen(1);
+
+            Log($"Server created on port {port}");
+
+            while (true)
+            {
+                try
+                {
+                    using var clientSocket = await serverSocket.AcceptAsync();
+                    Log($"Client socket accepted");
+
+                    var dataReceived = new List<byte>();
+                    var buffer = new byte[4096];
+                    while (clientSocket.Connected)
+                    {
+                        var dataRead = clientSocket.Receive(buffer);
+                        if (dataRead == 0) continue;
+                        dataReceived.AddRange(buffer.Take(dataRead));
+
+                        var zeroIndex = dataReceived.IndexOf(0);
+                        while (zeroIndex != -1)
+                        {
+                            ExecuteCommand(dataReceived, zeroIndex, clientSocket);
+                            dataReceived = dataReceived.Skip(zeroIndex + 1).ToList();
+                            zeroIndex = dataReceived.IndexOf(0);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log(ex);
+                }
+            }
+        }
+
+        private static void ExecuteCommand(List<byte> dataReceived, int endIndex, Socket socket)
+        {
+            var commandSpan = CollectionsMarshal.AsSpan(dataReceived)[..endIndex];
+            var command = Encoding.UTF8.GetString(commandSpan);
+            var words = command.Split(' ');
+            switch (words[0])
+            {
+                case "guid":
+                    var count = int.Parse(words[1], CultureInfo.InvariantCulture);
+                    Log($"Generating {count} GUIDs");
+                    var guids = Enumerable.Range(0, count).Select(_ => Guid.NewGuid()).ToList();
+                    foreach (var guid in guids)
+                    {
+                        var data = Encoding.UTF8.GetBytes(guid.ToString());
+                        socket.Send(data);
+                    }
+                    Log($"{count} GUIDs sent successfully");
+                    break;
+                default: throw new Exception($"Unknown command: {command}");
+            }
+        }
+    }
+}
---
+++ Speaker/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Speaker
+{
+    class Program
+    {
+        private static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void Log(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        
+        static int Main(string[] args)
+        {
+            int port = args.Length == 0 ? 8222 : int.Parse(args[0], CultureInfo.InvariantCulture);
+            bool stopAtRandom = true;
+            
+            const int guidCount = 2000;
+            var rand = new Random();
+            try
+            {
+                while (true)
+                {
+                    int disconnectAfter = stopAtRandom ? rand.Next(guidCount) : guidCount;
+                    Log($"Disconnecting after receiving {disconnectAfter} out of {guidCount} GUIDs");
+
+                    var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    var endPoint = new IPEndPoint(IPAddress.Loopback, port);
+
+                    var command = $"guid {guidCount}\0";
+                    socket.Connect(endPoint);
+                    // var array = new List<byte>();
+                    // for (int i = 0; i < 150; ++i)
+                        // array.AddRange(Encoding.UTF8.GetBytes(command));
+                    socket.SendAsync(Encoding.UTF8.GetBytes(command), 0);
+
+                    Task.Run(() =>
+                    {
+                        try
+                        {
+                            var guids = new List<Guid>();
+                            var buffer = new byte[1024];
+                            var dataReceived = new List<byte>();
+                            while (guids.Count < disconnectAfter)
+                            {
+                                var byteCount = socket.Receive(buffer);
+                                dataReceived.AddRange(buffer.Take(byteCount));
+                                while (dataReceived.Count >= 36)
+                                {
+                                    var span = CollectionsMarshal.AsSpan(dataReceived)[..36];
+                                    var datum = Encoding.UTF8.GetString(span);
+                                    var guid = Guid.Parse(datum);
+                                    guids.Add(guid);
+
+                                    dataReceived = dataReceived.Skip(36).ToList();
+                                }
+                            }
+                            
+                            Log("Read success");
+                        }
+                        catch (SocketException)
+                        {
+                            Log("Read terminated");
+                        }
+                    });
+                    
+                    Thread.Sleep(1);
+                    
+                    Log("Closing the socket");
+                    socket.Close(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+                return 1;
+            }
+        }
+    }
+}
